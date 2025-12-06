7725fbc023bea003660aede6f2fa24ffd046c174
Ren Tanaka <rentanaka15@icloud.com>
2025-12-06 23:20:34 +0900

    temp

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -195,7 +195,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     name: {
-        fontSize: 16,
+        fontSize: 14,
         fontWeight: 'bold',
         color: '#1F2937',
         marginBottom: 2,
